e72f604e46533bd3d22b7b5a5db69bb056c6bf19
Amir Fakhrullah <amirfakhrullah96@gmail.com>
2020-12-09 13:43:08 +0800

    dictionary to store information of the job

---
+++ job_description.py
@@ -0,0 +1,10 @@
+def job_description(role):
+    job_desc = {
+        '1. Role':'',
+        '2. Company':'',
+        '3. Posted':'',
+        '4. To apply':''
+    }
+
+    job_desc['1. Role'] = role
+    return job_desc
