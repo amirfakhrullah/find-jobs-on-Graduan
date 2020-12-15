953740796dfff00a9b45f5cbda129a5ff4b3d395
Amir Fakhrullah <amirfakhrullah96@gmail.com>
2020-12-15 16:35:23 +0800

    bug fixed

--- graduan_jobs.py
+++ graduan_jobs.py
@@ -15,6 +15,8 @@ driver = webdriver.Chrome()
 url = 'https://graduan.com/jobs?page=1'
 driver.get(url)
 
+
+# to loop between pages
 page = 1
 loop = True
 while loop:
@@ -29,30 +31,34 @@ while loop:
     jobs = soup.find_all('div', {'class':'item'})
 
     if len(jobs) == 0: break
-
+    # to loop between jobs shown in the page
     for job in jobs:
         role = job.find('h4', {'class':'margin-bottom-5'}).text
         check = True
         for word in search:
+            # to make sure the job search matches the title of the job
             if word.lower() not in role.lower():
                 check = False
                 break
         if check == True:
+            # store the jobs in a dictionary from job_description.py
             job_desc = job_description(role)
             try:
+                # store the name of the comnpany
                 company = job.find('p').text
                 job_desc['2. Company'] = company
             except:
                 Exception()
-
+            #store the date posted
             posted = job.find('span', {'class':'moment-humanize'}).text
             job_desc['3. Posted'] = posted
                 
-
+            # store the href link to apply for the job
             apply = job.find('a', href=True)
             job_desc['4. To apply'] = apply['href']
             pprint.pprint(job_desc)
-            
+    
+    # to move to the next page
     page += 1
     try:
         driver.get(driver.current_url[:-1] +  str(page))
